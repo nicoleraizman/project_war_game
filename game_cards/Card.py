3c6bf76032cce0814e683d5d31cae62e81f76e29
ybibt <ybitman20@gmail.com>
2022-09-07 12:00:09 +0300

    version two of card

--- game_cards/Card.py
+++ game_cards/Card.py
@@ -2,35 +2,40 @@ class Card:
 
     def __init__(self, suit, value):
         # Check type of suit
-        if type(suit) != int:
-            raise TypeError('invalid type of suit,must be int')
+        if type(suit) != str:
+            raise TypeError('invalid type of suit,must be str')
         # Check type of value
-        if type(value) != int:
-            raise TypeError('invalid type of value,must be int')
+        if type(value) != int or str:
+            raise TypeError('invalid type of value,must be int or str')
 
         self.suit = suit
+        self.cards_suit = {'Diamond':1, 'Spade':2, 'Heard':3, 'Club':4}
 
         self.value = value
+        self.cards_value = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
     """checking which value high then other and if they equal
         he checking which suit high then other"""
     def __gt__(self, other):
         if isinstance(other, Card):
-            if self.value > other.value:
+            if self.cards_value[self.value] > other.cards_value[self.value]:
                 return True
-            elif self.value == other.value:
-                if self.suit > other.suit:
+            elif self.cards_value[self.value] == other.cards_value[self.value]:
+                if self.cards_suit[self.suit] > other.cards_suit[self.suit]:
                     return True
                 else:
                     return False
             else:
                 return False
         else:
-            raise TypeError('it must be ')
+            raise TypeError('it must be class Card')
 
     """checking which if the value is equal"""
     def __eq__(self, other):
-        if self.value == other:
-            return True
+        if isinstance(other, Card):
+            if self.cards_value[self.value] == other.cards_value[self.value]:
+                return True
+            else:
+                return False
         else:
-            return False
+            raise TypeError('it must be class Card')
